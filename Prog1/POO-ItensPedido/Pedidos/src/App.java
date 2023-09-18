e75d454220f371f5fd766f45d189e70c5a1aa44c
Lucas Bitello <lucasbitellodasilva@outlook.com>
2023-09-18 13:20:02 -0300

    Correção bugs.

--- Prog1/POO-ItensPedido/Pedidos/src/App.java
+++ Prog1/POO-ItensPedido/Pedidos/src/App.java
@@ -35,7 +35,7 @@ public class App {
 
                 continue;
             } else if (codSelecionado == -2) {
-                
+
                 ItemPedido[] pedidos = pedidoCliente.getItensPedido();
                 if (pedidos.length == 0) {
                     System.out.println("Seu carrinho está vazio.");
@@ -43,21 +43,23 @@ public class App {
                     System.out.println("Seu carrinho possui:");
                     for (int i = 0; i < pedidos.length; i++) {
                         System.out.println("Item " + (i + 1) + ": " + pedidos[i].getQuantidade() + " "
-                                + pedidos[i].getProduto().getNome() + "(s), total de R$ " + (pedidos[i].getQuantidade() * pedidos[i].getProduto().getValor()));
+                                + pedidos[i].getProduto().getNome() + "(s), total de R$ "
+                                + (pedidos[i].getQuantidade() * pedidos[i].getProduto().getValor()));
                     }
                 }
 
                 continue;
-            }else if (codSelecionado == -3) {
-                
+            } else if (codSelecionado == -3) {
+
                 ItemPedido[] pedidos = pedidoCliente.getItensPedido();
                 if (pedidos.length == 0) {
                     System.out.println("Desculpa por não termos oque procurava iremos tentar melhorar.");
                 } else {
                     System.out.println("Seu carrinho ficou assim:");
                     for (int i = 0; i < pedidos.length; i++) {
-                        System.out.println("Item " + (i + 1) + ": " + pedidos[i].getQuantidade()
-                                + pedidos[i].getProduto().getNome() + "(s), total de R$ " + (pedidos[i].getQuantidade() * pedidos[i].getProduto().getValor()));
+                        System.out.println("Item " + (i + 1) + ": " + pedidos[i].getQuantidade() + " "
+                                + pedidos[i].getProduto().getNome() + "(s), total de R$ "
+                                + (pedidos[i].getQuantidade() * pedidos[i].getProduto().getValor()));
                     }
                     pedidoCliente.showValorTotal();
                     System.out.println("Pix: +55 (49) 98848-7168");
@@ -67,31 +69,38 @@ public class App {
                 break;
             }
 
-
-            for (int i = 0; i < arrProdutos.length; i++) {
-                boolean isItemAddJa = false;
+            boolean isItemAddJa = false;
                 if (pedidoCliente.getItensPedido().length >= 1) {
                     for (int j = 0; j < pedidoCliente.getItensPedido().length; j++) {
-                        System.out.println(codSelecionado + "çlsmflçmsdçlfmsçlm");
                         if (pedidoCliente.getItensPedido()[j].getProduto().getCodigo() == codSelecionado) {
+                            isItemAddJa = true;
                             System.out.println("Você já selecionou " + pedidoCliente.getItensPedido()[j].getQuantidade()
                                     + " " + pedidoCliente.getItensPedido()[j].getProduto().getNome()
                                     + "(s) deseja incluir mais quantas (0 para nao incluir mais nenhuma):");
                             int maisQtde = scanQuantia.nextInt();
+                            if (maisQtde + pedidoCliente.getItensPedido()[j].getQuantidade() > pedidoCliente.getItensPedido()[j].getProduto().getQtdeEstoque()) {
+                                System.out.println("Quantia informada maior que a disponivel, tente novamente;");
+                                break;
+                            }
                             pedidoCliente.getItensPedido()[j]
                                     .alterarQuantidade(maisQtde);
-                            isItemAddJa = true;
                             break;
                         }
                     }
-                }
-                ;
+                };
+
+            for (int i = 0; i < arrProdutos.length; i++) {
                 if (!isItemAddJa && codSelecionado == arrProdutos[i].getCodigo()) {
                     ItemPedido newItemPedido = new ItemPedido(arrProdutos[i]);
 
                     System.out.println("Temos em estoque " + arrProdutos[i].getQtdeEstoque() + " "
                             + arrProdutos[i].getNome() + "s, quantas desejas: ");
-                    newItemPedido.alterarQuantidade(scanQuantia.nextInt());
+                    int qtdeEsc = scanQuantia.nextInt();
+                    if (qtdeEsc > newItemPedido.getProduto().getQtdeEstoque()) {
+                        System.out.println("Quantia informada maior que a disponivel, tente novamente;");
+                        break;
+                    }
+                    newItemPedido.alterarQuantidade(qtdeEsc);
                     pedidoCliente.adicionarProdutosAoPedido(newItemPedido);
                     break;
                 }
@@ -99,8 +108,6 @@ public class App {
 
             System.out.println("#####################");
         }
-
-
 
         scanCode.close();
         scanQuantia.close();
